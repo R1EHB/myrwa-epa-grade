e3cf14da8f5d63a6553fcb0d496d4bec5d1d314f
Jeff Walker <walker.jeff.d@gmail.com>
2015-07-07 17:53:44 -0400

    update database version

--- load_data.R
+++ load_data.R
@@ -1,6 +1,6 @@
 library(dplyr)
 library(myrwaR)
-ch <- db_connect('H:/Dropbox/Work/mystic/db/MysticDB_20150603.accdb')
+ch <- db_connect('H:/Dropbox/Work/mystic/db/MysticDB_20150707.accdb')
 
 df <- db_results(ch)
 df <- filter(df, SampleTypeID=="S",
